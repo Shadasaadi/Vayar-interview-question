cf7e696babc29fbf0fee1385d8e618f3607be50a
Shada Saadi <84487686+Shadasaadi@users.noreply.github.com>
2023-01-08 10:30:54 +0200

    Add files via upload

---
+++ vayar-sumOfwo.py
@@ -0,0 +1,16 @@
+def sumOfTwo( self,nums,target) :
+        #declaring a dictionary to keep track of all the values
+        dictionary = {}
+        
+        # iterating over the entire array
+        for i in range(len(nums)):
+            #subtracting the num from the target to search for its pair
+            secondNumber = target-nums[i]
+            #if the pair is found, return the index of the both numbers
+            if(secondNumber in dictionary.keys()):
+                secondIndex = nums.index(secondNumber)
+                # making sure we don't get the same index twice 
+                if(i != secondIndex):
+                    return sorted([i, secondIndex])
+                
+            dictionary.update({nums[i]: i})
